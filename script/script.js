1c7192440343ea091eda39cdbfe04ac2c04b131e
EndreusSre <endreus.valerio@gmail.com>
2025-09-05 01:57:49 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -1,14 +1,26 @@
 const bntEnviar = document.querySelector("form button");
 const nome = document.querySelector("#nome")
 const email = document.querySelector("#email");
-const mensagem = document.querySelector("#mensagem")
+const mensagem = document.querySelector("#mensagem");
+const statusMensagem = document.querySelector("#status-mensagem");
 
 bntEnviar.addEventListener("click", function(e) {
     e.preventDefault();
     if (nome.value.trim() !== "" && email.value.trim() !== "" && mensagem.value.trim() !== "") {
-        alert("Sua resposta foi enviada")
+        statusMensagem.textContent = "mensagem enviada👍"
+        statusMensagem.style.color = "black"
+        statusMensagem.style.backgroundColor = "white"
+        statusMensagem.style.border = "solid 1px black"
+        statusMensagem.style.borderRadius = "5px"
+        statusMensagem.style.width = "300px"
     } else {
-       alert("Preencha os campos")
+       statusMensagem.textContent = "preencha os campos👎"
+       statusMensagem.style.color = "red"
+       statusMensagem.style.color = "black"
+        statusMensagem.style.backgroundColor = "white"
+        statusMensagem.style.border = "solid 1px black"
+        statusMensagem.style.borderRadius = "5px"
+        statusMensagem.style.width = "300px"
     }
     
 })
